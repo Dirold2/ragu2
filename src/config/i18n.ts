2d8ff9a1d1728a04316e107ab4e1cc953ec5f35f
dirold2 <mr.did199@mail.ru>
2024-09-19 20:42:08 +0000

    beta v0.0.7.1

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -1,13 +1,13 @@
-import i18next from 'i18next';
+// import i18next from 'i18next';
 
-export const initI18n = async () => {
-  await i18next.init({
-    lng: 'ru',
-    fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false,
-    },
-  });
-};
+// export const initI18n = async () => {
+//   await i18next.init({
+//     lng: 'ru',
+//     fallbackLng: 'en',
+//     interpolation: {
+//       escapeValue: false,
+//     },
+//   });
+// };
 
-export const t = i18next.t.bind(i18next);
+// export const t = i18next.t.bind(i18next);
